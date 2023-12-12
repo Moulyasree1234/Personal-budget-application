f53f36e36417204eac67e444eb7abb8bfb63a0ac
Moulyasree1234 <143620769+Moulyasree1234@users.noreply.github.com>
2023-12-11 21:52:26 -0500

    added a line graph

--- personal-budget-vitereact/src/pages/DashboardPage.jsx
+++ personal-budget-vitereact/src/pages/DashboardPage.jsx
@@ -3,7 +3,7 @@ import { Box, Grid } from "@mui/material";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { BarChart, PieChart } from "@mui/x-charts";
+import { BarChart, LineChart, PieChart } from "@mui/x-charts";
 
 const DashboardPage = () => {
   const [monthlyBudget, setMonthlyBudget] = useState();
@@ -151,20 +151,17 @@ const DashboardPage = () => {
                 />
             </Grid>
             <Grid item xs={6} sx={{ marginBottom: "10vh" }}>
-                <PieChart
-                  series={[
-                    {
-                      data: [
-                        { id: 0, value: 10, label: "series A" },
-                        { id: 1, value: 15, label: "series B" },
-                        { id: 2, value: 20, label: "series C" },
-                      ],
-                    },
-                  ]}
-                  width={400}
-                  height={200}
-                />
-              </Grid>
+              <LineChart
+                xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+                series={[
+                  {
+                    data: [2, 5.5, 2, 8.5, 1.5, 5],
+                  },
+                ]}
+                width={500}
+                height={300}
+              />
+            </Grid>
             </>
           )}
       </Grid>
